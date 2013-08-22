211d3a353c56186440d43e2bc5a8ca3e83d214fa
Jon Aizlewood <jon@clearleft.com>
2013-08-22 17:36:01 +0100

    Tweakages

--- site/snippets/author.php
+++ site/snippets/author.php
@@ -2,3 +2,7 @@
 	<img class="avatar" src="/assets/images/avatar.jpg" alt="avatar" />
 	<strong><a href="http://twitter.com/aizlewood" rel="author">@Aizlewood</a></strong><br><i>wrote this article on</i><br><strong><time datetime="<?php echo $page->date('c') ?>" pubdate="pubdate"><?php echo $page->date('F j, Y') ?></time></strong>
 </div>
+
+<div class="blogmenu">
+		<?php snippet('prevnext--work') ?>
+</div>	
